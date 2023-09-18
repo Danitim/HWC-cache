e1d9b771b004923d2c2310fc42c04cf414146219
Danitim <danitim4@gmail.com>
2023-09-18 23:40:32 +0300

    Fixed: Perfect caching algorithm, test data file

--- Ideal_Cache/ideal_cache.cpp
+++ Ideal_Cache/ideal_cache.cpp
@@ -6,10 +6,6 @@ int icache_hits(size_t cache_size, int buf_len, std::vector<int> requests) {
     /*Create unordered_map filled with iterators to the first appearances of requests vector,
     if the element does not have future appearances, iterator is equal to vector.end()*/
     std::unordered_map<int, std::vector<int>::iterator> next_appearance;
-    for (int i = buf_len-1; i >= 0; i--) {
-        next_appearance[requests[i]] = std::find(requests.begin()+i+1, requests.end(), requests[i]);
-    }
-
     std::list<int> cache;
     for (int i=0; i<buf_len; i++) {
         int cur = requests[i];
@@ -21,12 +17,14 @@ int icache_hits(size_t cache_size, int buf_len, std::vector<int> requests) {
         if (cache.size() < cache_size) { cache.push_back(cur); continue; }
 
         next_appearance[cur] = std::find(requests.begin()+i+1, requests.end(), cur);
+        if (next_appearance[cur] == requests.end()) continue;
 
         //We must choose the element to delete
         int max_dist = next_appearance[cur] - requests.begin();
         int value_to_delete = cur;
         std::list<int>::iterator it;
         for (it=cache.begin(); it!=cache.end(); ++it) {
+            next_appearance[*it] = std::find(requests.begin()+i+1, requests.end(), *it);
             std::vector<int>::iterator next = next_appearance[*it];
 
             //If we find a value that won't occur again, we delete it.
@@ -40,6 +38,7 @@ int icache_hits(size_t cache_size, int buf_len, std::vector<int> requests) {
         }
 
         //Delete the unwanted element and add the desired one to the cache
+        if (value_to_delete == cur) continue;
         cache.remove(value_to_delete);
         cache.push_back(cur);
     }
