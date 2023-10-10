b6c351bbab62ebf97ca5d5cc95075a62cc8ac66d
Danitim <danitim4@gmail.com>
2023-10-11 00:17:02 +0300

    Modified: delete unnecessary dependencies in ideal_cache.hpp

--- Ideal_Cache/ideal_cache.hpp
+++ Ideal_Cache/ideal_cache.hpp
@@ -3,7 +3,5 @@
 #include <set>
 #include <unordered_set>
 #include <unordered_map>
-#include <iostream>
-#include <list>
 
 int icache_hits(size_t cache_size, int buf_len, std::vector<int> requests);
