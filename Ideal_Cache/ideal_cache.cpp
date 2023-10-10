7a55fbb7fbac972f1ab617b20d48b6f8f595aacf
Danitim <danitim4@gmail.com>
2023-10-10 21:24:59 +0300

    Modified: Perfect caching algorithm optimized

--- Ideal_Cache/ideal_cache.cpp
+++ Ideal_Cache/ideal_cache.cpp
@@ -1,47 +1,55 @@
 #include "ideal_cache.hpp"
 
+
+// Record the next page access time if it is requested in the future
+static void next_appearance(int index, int request, 
+                             const std::vector<int> &requests, 
+                             std::unordered_map<int, std::set<std::pair<int, int>>::iterator> &iterators, 
+                             std::set<std::pair<int, int>> &next_appear);
+
 int icache_hits(size_t cache_size, int buf_len, std::vector<int> requests) {
     int hits = 0;
 
-    /*Create unordered_map filled with iterators to the first appearances of requests vector,
-    if the element does not have future appearances, iterator is equal to vector.end()*/
-    std::unordered_map<int, std::vector<int>::iterator> next_appearance;
-    std::list<int> cache;
-    for (int i=0; i<buf_len; i++) {
-        int cur = requests[i];
-
-        //If it is a hit, we count it and continue
-        if (std::find(cache.begin(), cache.end(), cur) != cache.end()) { hits ++; continue; }
-
-        //If the cache is not full, we add current element to the cache and continue
-        if (cache.size() < cache_size) { cache.push_back(cur); continue; }
-
-        next_appearance[cur] = std::find(requests.begin()+i+1, requests.end(), cur);
-        if (next_appearance[cur] == requests.end()) continue;
-
-        //We must choose the element to delete
-        int max_dist = next_appearance[cur] - requests.begin();
-        int value_to_delete = cur;
-        std::list<int>::iterator it;
-        for (it=cache.begin(); it!=cache.end(); ++it) {
-            next_appearance[*it] = std::find(requests.begin()+i+1, requests.end(), *it);
-            std::vector<int>::iterator next = next_appearance[*it];
-
-            //If we find a value that won't occur again, we delete it.
-            if (next == requests.end()) { value_to_delete = *it; break; }
-
-            //Otherwise, we look for the element that occurs most recently.
-            if ((next - requests.begin()) > max_dist) {
-                max_dist = next - requests.begin();
-                value_to_delete = *it;
+    // A set for quickly determining whether a page is in the cache.
+    std::unordered_set<int> cache; 
+    // A dictionary to store iterators to 'next_appear' elements for each page.
+    std::unordered_map<int, std::set<std::pair<int, int>>::iterator> iterators;
+    // A set to store the next access time for each page. It is automatically sorted by access time.
+    std::set<std::pair<int, int>> next_appear; 
+    
+    for(int i = 0; i < buf_len; i++) {
+        // If the page is already in cache.
+        if(cache.find(requests[i]) != cache.end()) {
+            hits++;
+            next_appear.erase(iterators[requests[i]]);
+            next_appearance(i, requests[i], requests, iterators, next_appear);
+        } else {
+            // If there is space in the cache, add the page.
+            if(cache.size() < cache_size) {
+                cache.insert(requests[i]);
+                next_appearance(i, requests[i], requests, iterators, next_appear);
+            } else {
+                // If the cache is full, find the page with the furthest next appearance and delete it.
+                auto farthest = next_appear.rbegin();
+                cache.erase(farthest->second);
+                iterators.erase(farthest->second);
+                next_appear.erase(std::next(farthest).base());
+                cache.insert(requests[i]);
+                next_appearance(i, requests[i], requests, iterators, next_appear);
             }
         }
-
-        //Delete the unwanted element and add the desired one to the cache
-        if (value_to_delete == cur) continue;
-        cache.remove(value_to_delete);
-        cache.push_back(cur);
     }
-
     return hits;
+}
+
+static void next_appearance(int index, int request, 
+                             const std::vector<int> &requests, 
+                             std::unordered_map<int, std::set<std::pair<int, int>>::iterator> &iterators, 
+                             std::set<std::pair<int, int>> &next_access) {
+    int next = index + 1;
+    while(next < requests.size() && requests[next] != request) next++;
+    
+    if (next != requests.size()) {
+        iterators[request] = next_access.insert({next, request}).first;
+    }
 }
--- Ideal_Cache/ideal_cache.hpp
+++ Ideal_Cache/ideal_cache.hpp
@@ -1,8 +1,7 @@
-#pragma once
-
 #include <algorithm>
 #include <vector>
-#include <list>
+#include <set>
+#include <unordered_set>
 #include <unordered_map>
 
 int icache_hits(size_t cache_size, int buf_len, std::vector<int> requests);
